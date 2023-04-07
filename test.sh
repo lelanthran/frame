a47e68ddad4dab989d411544a2d7925cb400543f
lelanthran <lelanthran@gmail.com>
2023-04-07 09:57:24 +0200

    Updates test script for gdb support
    WIP: Working on removal, which is dangerous.
    
    Ensuring that pop() works. Since it uses delete(), this is dangerous
    (might accidentally chdir() to real filesystem root and issue a
    recursive delete).

--- test.sh
+++ test.sh
@@ -12,50 +12,79 @@ die () {
    exit -1
 }
 
+export NC="\e[0m"
+export INV="\e[7m"
+export RED="\e[31m"
+export GREEN="\e[32m"
+export BLUE="\e[34m"
+export CYAN="\e[36m"
+export YELLOW="\e[33m"
+
+export HI_ON="${RED}${INV}"
+export STMT_NUM=0
+
+if [ -z "$DEBUG" ]; then
+   export DEBUG=-1
+fi
+
+
+execute () {
+   echo -ne "${RED}Executing${NC} ${GREEN}$STMT_NUM:${NC} "
+   echo -e "${BLUE}$@${NC}"
+   if [ ! -z "$VG" ]; then
+      valgrind --leak-check=full --show-leak-kinds=all $@
+      export RET=$?
+      export STMT_NUM=$(($STMT_NUM + 1))
+      return $RET
+      return $?
+   fi
+
+   if [ "$DEBUG" -eq "$STMT_NUM" ]; then
+      gdb --args $@
+      export RET=$?
+      export STMT_NUM=$(($STMT_NUM + 1))
+      return $RET
+   fi
+
+   export STMT_NUM=$(($STMT_NUM + 1))
+   $@
+}
+
 rm -rf $DBPATH
-$VG $PROG --dbpath=$DBPATH init || die failed to init
-$VG $PROG --dbpath=$DBPATH status || die failed status
-$VG $PROG --dbpath=$DBPATH history || die failed history
-echo '--------------------------------'
+execute $PROG --dbpath=$DBPATH init || die failed to init
+execute $PROG --dbpath=$DBPATH status || die failed status
+execute $PROG --dbpath=$DBPATH history || die failed history
 
-$VG $PROG --dbpath=$DBPATH replace --message="Root: replacement" || die failed replace
-$VG $PROG --dbpath=$DBPATH status || die failed status
-$VG $PROG --dbpath=$DBPATH history || die failed history
-echo '--------------------------------'
+execute $PROG --dbpath=$DBPATH replace --message="Root: replacement" || die failed replace
+execute $PROG --dbpath=$DBPATH status || die failed status
+execute $PROG --dbpath=$DBPATH history || die failed history
 
-$VG $PROG --dbpath=$DBPATH push one --message="One: message one" || die failed push
-$VG $PROG --dbpath=$DBPATH status || die failed status
-$VG $PROG --dbpath=$DBPATH history || die failed history
-echo '--------------------------------'
+execute $PROG --dbpath=$DBPATH push one --message="One: message one" || die failed push
+execute $PROG --dbpath=$DBPATH status || die failed status
+execute $PROG --dbpath=$DBPATH history || die failed history
 
-$VG $PROG --dbpath=$DBPATH append --message="\nMessage: 1\n" || die failed append
-$VG $PROG --dbpath=$DBPATH status || die failed status
-$VG $PROG --dbpath=$DBPATH history || die failed history
-echo '--------------------------------'
+execute $PROG --dbpath=$DBPATH append --message="\nMessage: 1\n" || die failed append
+execute $PROG --dbpath=$DBPATH status || die failed status
+execute $PROG --dbpath=$DBPATH history || die failed history
 
-$VG $PROG --dbpath=$DBPATH up || die failed uptree
-$VG $PROG --dbpath=$DBPATH status || die failed status
-$VG $PROG --dbpath=$DBPATH history || die failed history
-echo '--------------------------------'
+execute $PROG --dbpath=$DBPATH up || die failed uptree
+execute $PROG --dbpath=$DBPATH status || die failed status
+execute $PROG --dbpath=$DBPATH history || die failed history
 
-$VG $PROG --dbpath=$DBPATH push two --message="Two: message two" || die failed push
-$VG $PROG --dbpath=$DBPATH status || die failed status
-$VG $PROG --dbpath=$DBPATH history || die failed history
-echo '--------------------------------'
+execute $PROG --dbpath=$DBPATH push two --message="Two: message two" || die failed push
+execute $PROG --dbpath=$DBPATH status || die failed status
+execute $PROG --dbpath=$DBPATH history || die failed history
 
-$VG $PROG --dbpath=$DBPATH switch root/one || die failed switch
-$VG $PROG --dbpath=$DBPATH status || die failed status
-$VG $PROG --dbpath=$DBPATH history || die failed history
-echo '--------------------------------'
+execute $PROG --dbpath=$DBPATH switch root/one || die failed switch
+execute $PROG --dbpath=$DBPATH status || die failed status
+execute $PROG --dbpath=$DBPATH history || die failed history
 
-$VG $PROG --dbpath=$DBPATH push three --message="three: three" || die failed push
-$VG $PROG --dbpath=$DBPATH status || die failed status
-$VG $PROG --dbpath=$DBPATH history || die failed history
-echo '--------------------------------'
+execute $PROG --dbpath=$DBPATH push three --message="three: three" || die failed push
+execute $PROG --dbpath=$DBPATH status || die failed status
+execute $PROG --dbpath=$DBPATH history || die failed history
 
-$VG $PROG --dbpath=$DBPATH pop  || die failed pop
-$VG $PROG --dbpath=$DBPATH status || die failed status
-$VG $PROG --dbpath=$DBPATH history || die failed history
-echo '--------------------------------'
+execute $PROG --dbpath=$DBPATH pop  || die failed pop
+execute $PROG --dbpath=$DBPATH status || die failed status
+execute $PROG --dbpath=$DBPATH history || die failed history
 
 
